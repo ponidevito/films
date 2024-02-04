a0f1268c6c7967377a460cae329f8b9176c36604
ponidevito <olegmama@gmail.com>
2024-02-04 16:07:55 +0200

    add style and js to modal registartion

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -451,9 +451,67 @@ async function uploadImage(file) {
 // bindModal('.modal__btn', '.modal__wrapper', '.modal__close')
 
 
-const modalBtnEnter = document.querySelector ('.modal__button')
-const modalTitle = document.querySelector ('.modal__title')
+// test 1
 
+// const modalBtnEnter = document.querySelector ('.modal__button')
+// const modalTitle = document.querySelector ('.modal__title')
+
+
+// function bindModal(trigger, modal, close) {
+//   trigger = document.querySelector(trigger);
+//   modal = document.querySelector(modal);
+//   close = document.querySelector(close);
+//   const body = document.body;
+
+//   trigger.addEventListener('click', e => {
+//     e.preventDefault();
+//     modal.style.display = 'flex';
+//     body.classList.add('locked');
+//   });
+
+//   close.addEventListener('click', () => {
+//     modal.style.display = 'none';
+//     body.classList.remove('locked');
+//   });
+
+//   modal.addEventListener('click', e => {
+//     if (e.target === modal) {
+//       modal.style.display = 'none';
+//       body.classList.remove('locked');
+//     }
+//   });
+// }
+
+// function toggleRegistrationForm() {
+//   const loginForm = document.getElementById('loginForm');
+//   const registrationForm = document.getElementById('registrationForm');
+//   const registrationLink = document.getElementById('registrationLink');
+
+//   loginForm.style.display = 'none';
+//   modalBtnEnter.style.display = 'none';
+//   registrationForm.style.display = 'flex';
+//   registrationLink.textContent = 'Увійти';
+//   modalTitle.textContent = 'Реєстрація';
+//   registrationLink.onclick = toggleLoginForm;
+// }
+
+// function toggleLoginForm() {
+//   const loginForm = document.getElementById('loginForm');
+//   const registrationForm = document.getElementById('registrationForm');
+//   const registrationLink = document.getElementById('registrationLink');
+
+//   loginForm.style.display = 'flex';
+//   registrationForm.style.display = 'none';
+//   registrationLink.textContent = 'Зареєструватись';
+//   registrationLink.onclick = toggleRegistrationForm;
+// }
+
+// // Initialize modal with default content
+// bindModal('.modal__btn', '.modal__wrapper', '.modal__close');
+
+
+const modalBtnEnter = document.querySelector ('.modal-enter')
+const modalTitle = document.getElementById ('title')
 
 function bindModal(trigger, modal, close) {
   trigger = document.querySelector(trigger);
@@ -484,31 +542,38 @@ function toggleRegistrationForm() {
   const loginForm = document.getElementById('loginForm');
   const registrationForm = document.getElementById('registrationForm');
   const registrationLink = document.getElementById('registrationLink');
+  const regButton = document.getElementById('reg-button'); // Додаємо клас reg-button
+
 
   loginForm.style.display = 'none';
   modalBtnEnter.style.display = 'none';
-  registrationForm.style.display = 'block';
+  registrationForm.classList.add('form-registration')
+  registrationForm.style.display = 'flex';
   registrationLink.textContent = 'Увійти';
-  modalTitle.textContent = 'Реєстрація';
+  registrationLink.style.textAlign = 'start';
   registrationLink.onclick = toggleLoginForm;
+  regButton.style.display = 'flex';
+  modalTitle.textContent = 'Зареєструватись';
 }
 
 function toggleLoginForm() {
   const loginForm = document.getElementById('loginForm');
   const registrationForm = document.getElementById('registrationForm');
   const registrationLink = document.getElementById('registrationLink');
+  const regButton = document.getElementById('reg-button'); // Додаємо клас reg-button
 
-  loginForm.style.display = 'block';
+  loginForm.style.display = 'flex';
+  registrationForm.classList.remove('form-registration')
   registrationForm.style.display = 'none';
   registrationLink.textContent = 'Зареєструватись';
+  modalTitle.textContent = 'Вхід';
+  registrationLink.style.textAlign = 'end';
   registrationLink.onclick = toggleRegistrationForm;
+  regButton.style.display = 'none';
 }
 
 // Initialize modal with default content
 bindModal('.modal__btn', '.modal__wrapper', '.modal__close');
-
-
-
 // // login
 
 let searchBox = document.querySelector(".header__box");
